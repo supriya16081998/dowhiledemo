550197bfd83162069a074217367bac65886a1b21
dell <dell@DESKTOP-KTJNK95>
2021-09-14 20:35:04 +0530

    Add project files.

---
+++ dowhiledemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dowhiledemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 11;
+            do
+            {
+                Console.WriteLine(i);
+                
+            } while (i <= 10);
+
+            Console.ReadLine();
+        }
+       
+    }
+}
